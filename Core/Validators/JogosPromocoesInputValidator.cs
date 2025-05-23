43a3c1df45b4aa0d6e7b8dd23ef485c45bf6469d
Ricardo Zitelli <iblue.rzoliveira@terceiros.mag.com.br>
2025-05-23 01:08:27 -0300

    feat: implementar CRUD de JogosPromoções
    - Criado endpoint JogosPromocoesController com operações de cadastro.
    - Adicionado record JogosPromocoesInput para entrada de dados.
    - Implementado validator JogosPromocoesInputValidator com validação de desconto maior que 0.
    - Criado repositório JogosPromocoesRepository.
    - Removida classe antiga JogosPromocoes desnecessária.
    - Ajustado Program.cs e FIAPCloudGamesApi.csproj para registrar os novos serviços e validadores.
    - Implementada validação para verificar se JogoId e PromocaoId existem na base antes de criar uma promoção.

---
+++ Core/Validators/JogosPromocoesInputValidator.cs
@@ -0,0 +1,23 @@
+﻿using Core.Input;
+using FluentValidation;
+
+namespace Core.Validators
+{
+    public class JogosPromocoesInputValidator : AbstractValidator<JogosPromocoesInput>
+    {
+        public JogosPromocoesInputValidator()
+        {
+            RuleFor(x => x.JogoId)
+                .GreaterThan(0)
+                .WithMessage("O Id do jogo deve ser maior que 0.");
+
+            RuleFor(x => x.PromocaoId)
+                .GreaterThan(0)
+                .WithMessage("O Id da promoção deve ser maior que 0.");
+
+            RuleFor(x => x.Desconto)
+                .GreaterThan(0)
+                .WithMessage("O desconto deve ser maior que 0.");
+        }
+    }
+}
