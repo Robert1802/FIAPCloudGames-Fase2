6b30dff5114f92f376467cbaae2c8598cadad502
Roberto Moura <roblm_@hotmail.com>
2025-07-22 10:46:38 -0300

    Logs sem informação vazia e inutil

--- FIAPCloudGames.Infrastructure/Migrations/20250722133027_Log-Fix.Designer.cs
+++ FIAPCloudGames.Infrastructure/Migrations/20250722133027_Log-Fix.Designer.cs
@@ -4,6 +4,7 @@ using FIAPCloudGames.Infrastructure.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FIAPCloudGames.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250722133027_Log-Fix")]
+    partial class LogFix
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
